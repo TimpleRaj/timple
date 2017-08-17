cb56e2373b7a73a4c47d58359db7a7d9b56f1d72
TimpleRaj <31107605+TimpleRaj@users.noreply.github.com>
2017-08-17 23:05:20 +0530

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -4,6 +4,15 @@ public class reverse
 {
   public static void main(String args[])
   {
-     
+    Scanner sc = new Scanner(System.in);
+     String s_str = sc.nextLine();
+     String res="";
+     char[] c_str = s_str.toCharArray();
+     int i,j,len=s_str.length();
+     for(i=len-1;i>=0;i--)
+     {
+        res=res+""+c_str[i];
+     }
+    System.out.println(res);
   }
 }
