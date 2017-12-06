e5dd34c645834c63ecfd90b57a63cde93788cde5
TimpleRaj <31107605+TimpleRaj@users.noreply.github.com>
2017-12-06 20:49:43 +0530

    Create NumState.cpp

---
+++ NumState.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter the Number : ";
+    cin>>num;
+    if(num==0)
+        cout<<"Zero";
+    else if (num>0)
+        cout<<"Positive";
+    else
+        cout<<"Negative";
+}
