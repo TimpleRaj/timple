965e67a7760b35b5e960b7224f798cb95da14668
TimpleRaj <31107605+TimpleRaj@users.noreply.github.com>
2017-12-06 23:11:26 +0530

    Update Max.cpp

--- Max.cpp
+++ Max.cpp
@@ -4,6 +4,12 @@ int main()
 {
    int num[3],i;
    cout<<"Enter the numbers : ";
+   while(!( cin>>num ))
+   {
+     cout<<"Not a Number"<<endl;
+     cin.clear();
+     cin.ignore(1000,'\n');
+   }
    for(i=0;i<3;i++)
      cin>>num[i];
    if(num[0]>num[1] && num[0]>num[2])
