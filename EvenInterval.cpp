fdb0cf90176d7926d6891a8211d2280d137ed1b5
TimpleRaj <31107605+TimpleRaj@users.noreply.github.com>
2017-12-07 00:26:14 +0530

    Create EvenInterval.cpp

---
+++ EvenInterval.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num[2],s=0,i=0;
+    cout<<"Enter a number : ";
+    for(i=0;i<2;i++)
+    {
+        while(!( cin>>num[i] ))
+        {
+         cout<<"Not a Number"<<endl;
+         cin.clear();
+         cin.ignore(1000,'\n');
+        }
+    }
+
+    for(i=num[0]+1;i<num[1];i++)
+    {
+        if(i%2==0)
+          cout<<i<<endl;
+    }
+}
